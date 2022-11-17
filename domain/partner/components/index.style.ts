1048333a0b4115e4634a53305fc36cd899dfb926
J1min <aoiharu41@gmail.com>
2022-11-17 14:32:47 +0900

    ADD :: 파트너스 Carousel 기능 완성

--- domain/partner/components/index.style.ts
+++ domain/partner/components/index.style.ts
@@ -1,16 +1,39 @@
 import Image from "next/image";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const ImageProvider = styled.div`
-  margin-right: 0.5rem;
+export const ImageProvider = styled.span`
+  margin: 0.5rem;
+  box-sizing: content-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const PartnerProvider = styled.div`
-  background-color: black;
-  display: flex;
+  width: 100%;
+  filter: drop-shadow(rgba(0, 0, 0, 0.04) 0px 2px 11px)
+    drop-shadow(rgba(50, 83, 198, 0.14) 0px 18px 40px);
   overflow: hidden;
+  display: flex;
 `;
 
-export const CustomImage = styled(Image)`
-  display: block;
+const rolling = keyframes` 
+  0% {
+    transform: translate3d(0px, 0px, 0px);
+  }
+  100% {
+    transform: translate3d(-25%, 0px, 0px);
+  }
+`;
+
+export const AnimationProvider = styled.div`
+  display: flex;
+  animation-duration: 30s;
+  animation-name: ${rolling};
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+  animation-direction: normal;
+  align-items: stretch;
+  flex-direction: row;
 `;
--- domain/partner/components/index.tsx
+++ domain/partner/components/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import type { NextPage } from "next";
-import { PartnerProvider, ImageProvider } from "./index.style";
+import {
+  PartnerProvider,
+  ImageProvider,
+  AnimationProvider,
+} from "./index.style";
 import Image from "next/image";
 import { SildeImage } from "../assets";
 
 const Partner: NextPage = () => {
   return (
     <PartnerProvider>
-      {new Array(10).fill(null).map((data, idx) => {
-        return (
-          <ImageProvider key={idx}>
-            <Image src={SildeImage} layout={"fixed"} alt={"파트너형님"} />
-          </ImageProvider>
-        );
-      })}
+      <AnimationProvider>
+        {new Array(80).fill(null).map((data, idx) => {
+          return (
+            <ImageProvider key={idx}>
+              <Image src={SildeImage} layout="fixed" alt="파트너" />
+            </ImageProvider>
+          );
+        })}
+      </AnimationProvider>
     </PartnerProvider>
   );
 };
