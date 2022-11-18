c31cdb868c16840e9ff87eb2c5e58b4c27af6e24
J1min <aoiharu41@gmail.com>
2022-11-18 22:55:30 +0900

    DELETE :: 뉴스 도메인 삭제

--- domain/news/components/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import type { NextPage } from "next";
-        
-const News: NextPage = () => {
-  return <>News</>
-};
-export default News;
-        
--- domain/news/container/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import type { NextPage } from "next";
-    
-const NewsContainer: NextPage = () => {
-  return <>News</>
-};
-export default NewsContainer;
-    
